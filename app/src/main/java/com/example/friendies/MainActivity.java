675eedb817b73ca76aa8c0dfae44269b14521e1e
veasnawt <veasnawt@gmail.com>
2020-03-10 15:35:35 +0700

    Added item to popular_recyclerview

--- app/src/main/java/com/example/friendies/MainActivity.java
+++ app/src/main/java/com/example/friendies/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.friendies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.friendies.adapter.PopularAdapter;
+import com.example.friendies.model.PopularModel;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView popular_recyclerview;
+    PopularAdapter adapter;
+    ArrayList<PopularModel> popularModels;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        popular_recyclerview = findViewById(R.id.popular_recyclerview);
+        popularModels = new ArrayList<>();
+
+        for(int i = 0; i < 5; i++){
+            PopularModel model = new PopularModel();
+            model.setImgCover(R.drawable.popularbook);
+            popularModels.add(model);
+
+        }
+
+        popular_recyclerview.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new PopularAdapter(this,popularModels);
+        popular_recyclerview.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/example/friendies/model/PopularModel.java
@@ -0,0 +1,22 @@
+package com.example.friendies.model;
+
+public class PopularModel {
+
+    int id, imgCover;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getImgCover() {
+        return imgCover;
+    }
+
+    public void setImgCover(int imgCover) {
+        this.imgCover = imgCover;
+    }
+}
